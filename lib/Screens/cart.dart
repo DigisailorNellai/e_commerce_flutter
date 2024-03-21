258aecb293d147422899a0d7ce5075f8ce6d9091
MohamedAbuthar <abutharskt@gmail.com>
2024-03-21 04:58:18 -0700

    editing the main pages

--- lib/Screens/cart.dart
+++ lib/Screens/cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/flash%20sale.dart';
 import 'package:flutter_application_1/Screens/notification.dart';
+import 'package:flutter_application_1/widgets/Celebrity_look.dart';
 import 'package:flutter_application_1/widgets/banner.dart';
 import 'package:flutter_application_1/widgets/designers.dart';
 import 'package:flutter_application_1/widgets/sliding.dart';
@@ -32,48 +33,44 @@ class _CartState extends State<Cart> {
           ],
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(children: [
-                Container(
-                  padding: const EdgeInsets.only(left: 20),
-                  width: 300,
-                  height: 60,
-                  child: TextField(
-                    onChanged: (value) {},
-                    decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: 'search',
-                        //filled: true,
-                        //contentPadding: const EdgeInsets.symmetric(vertical: 272, horizontal: 42),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                  ),
+          child: Column(children: [
+            Row(children: [
+              Container(
+                padding: const EdgeInsets.only(left: 20),
+                width: 300,
+                height: 60,
+                child: TextField(
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search),
+                      hintText: 'search',
+                      //filled: true,
+                      //contentPadding: const EdgeInsets.symmetric(vertical: 272, horizontal: 42),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20))),
                 ),
-                const SizedBox(
-                  width: 30,
-                ),
-                IconButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        iconColor: MaterialStatePropertyAll(Colors.purple)),
-                    icon: const Icon(Icons.sort))
-              ]),
-              const SizedBox(
-                height: 10,
               ),
-              const PromoCard2(),
-              const Designer(),
               const SizedBox(
-                height: 10,
+                width: 30,
               ),
-              Sliding(),
-              const SizedBox(
-                height: 3,
-              ),
-              const FlashSale(),
-            ],
-          ),
+              IconButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                      iconColor: MaterialStatePropertyAll(Colors.purple)),
+                  icon: const Icon(Icons.sort))
+            ]),
+            const SizedBox(
+              height: 10,
+            ),
+            const PromoCard2(),
+            const Designer(),
+            const SizedBox(
+              height: 10,
+            ),
+            Sliding(),
+            const CelebrityLook(),
+            const FlashSale()
+          ]),
         ),
       ),
     );
---
+++ lib/widgets/Celebrity_look.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CelebrityLook extends StatefulWidget {
+  const CelebrityLook({super.key});
+
+  @override
+  State<CelebrityLook> createState() => _CelebrityLookState();
+}
+
+class _CelebrityLookState extends State<CelebrityLook> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: SizedBox(
+            child: Column(children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Celebrity look',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          ),
+          Stack(children: [
+            Wrap(
+              spacing: 10.0,
+              runSpacing: 10.0,
+              children: [
+                Container(
+                  width: 156,
+                  height: 123,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage('assets/Rectangle 174image 1.png'),
+                  )),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Couple Dress',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 7),
+                  ),
+                ),
+                Container(
+                  width: 81,
+                  height: 123,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/Rectangle 175image 2.png'))),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Saree',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 7),
+                  ),
+                ),
+                Container(
+                  width: 81,
+                  height: 123,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/Rectangle 176image 3.png'))),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Mens Accessories',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 7),
+                  ),
+                ),
+                Container(
+                  width: 86,
+                  height: 83,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/Rectangle 179image 4.png'))),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Fasho Wear',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 7),
+                  ),
+                ),
+                Container(
+                  width: 65,
+                  height: 83,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/Rectangle 181image 5.png'))),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Jewels',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 7),
+                  ),
+                ),
+                Container(
+                  width: 81,
+                  height: 83,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage('assets/Rectangle 178image 6.png'))),
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Womens Accessories',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 6),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: Container(
+                    width: 90,
+                    height: 85,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage('assets/Rectangle 177image 7.png'))),
+                    alignment: Alignment.bottomCenter,
+                    child: const Text(
+                      'Mens Fasho',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 6),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ])
+        ])));
+  }
+}
